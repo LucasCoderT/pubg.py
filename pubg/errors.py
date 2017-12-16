92ecc171cecf7a8662dbf50b25823819376aaba7
Lucas Lukowski <lucas@iceteacity.com>
2017-12-15 18:46:35 -0700

    Created BaseException obj
    used when the api reports anything abnormal

--- pubg/errors.py
+++ pubg/errors.py
@@ -1,3 +1,10 @@
 class BadArgument(Exception):
     def __init__(self, argument):
         super().__init__("{0} is not a valid argument".format(argument))
+
+
+class BaseException(Exception):
+    def __init__(self, response):
+        self.code = response['code']
+        self.error = response['error']
+        super().__init__(self.error)
